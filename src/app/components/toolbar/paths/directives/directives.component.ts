b2365cb4f12988000bf31f527c067a77067a966c
MikaelSvanasbakken <31248055+MikaelSvanasbakken@users.noreply.github.com>
2022-08-12 13:11:10 +0200

    Added more content to directives tab
    added more content

--- src/app/components/toolbar/paths/directives/directives.component.ts
+++ src/app/components/toolbar/paths/directives/directives.component.ts
@@ -8,16 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent implements OnInit {
   isClicked: boolean = false;
   element: boolean = false;
+  ngStyleBool: boolean = false
+  mat: Array<string> = ['Pizza', 'Hamburger', 'Pasta']
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeNgStyle() {
+    this.ngStyleBool = !this.ngStyleBool
+  }
   showElement() {
     this.element = !this.element
   }
-  clicked() {
+  changeNgClass() {
     this.isClicked = !this.isClicked
-    console.log(this.isClicked)
   }
 }
